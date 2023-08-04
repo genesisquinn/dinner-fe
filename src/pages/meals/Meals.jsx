4c609b17953ba9057284532846e01e988c9bccc7
Genesis Quinn <genesis@quinns.io>
2023-08-04 10:15:56 -0400

    Removed code for Meals page

--- src/pages/meals/Meals.jsx
+++ src/pages/meals/Meals.jsx
@@ -1,28 +1,3 @@
-import { useState } from 'react';
-import './meals.css';
-import RecipeCard from "../../components/recipecard/recipecard";
-import Calendar from "../../components/calendar/calendar";
 
-const Meals = () => {
-    const [calendarRecipes, setCalendarRecipes] = useState([]);
 
-    const addToCalendar = (recipeTitle) => {
-        setCalendarRecipes([recipeTitle, ...calendarRecipes]);
-    };
 
-    const removeFromCalendar = (recipeTitle) => {
-        setCalendarRecipes(calendarRecipes.filter((title) => title !== recipeTitle));
-    };
-
-    return (
-        <div>
-            <h1>Meal Planning</h1>
-            <div>
-                <h2>Meal Planning Calendar</h2>
-                <Calendar calendarRecipes={calendarRecipes} />
-            </div>
-        </div>
-    );
-};
-
-export default Meals;
