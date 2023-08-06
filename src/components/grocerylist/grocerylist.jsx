7f0b984da09eae04738b0465c9f09aa829d60327
Genesis Quinn <genesis@quinns.io>
2023-08-06 19:22:18 -0400

    Ingredients are Parsed in List

--- src/components/grocerylist/grocerylist.jsx
+++ src/components/grocerylist/grocerylist.jsx
@@ -1,62 +1,3 @@
-// import { useState } from 'react';
-// import './grocerylist.css';
-// import DeleteBtn from '../deletebtn/deletebtn';
-
-// const GroceryList = () => {
-//     const [items, setItems] = useState([]);
-//     const [newItem, setNewItem] = useState('');
-
-//     const addItem = () => {
-//         if (newItem.trim() !== '') {
-//             setItems([...items, { name: newItem.trim(), crossed: false }]);
-//             setNewItem('');
-//         }
-//     };
-
-//     const toggleItem = (index) => {
-//         const updatedItems = [...items];
-//         updatedItems[index].crossed = !updatedItems[index].crossed;
-//         setItems(updatedItems);
-//     };
-
-//     const deleteAllItems = () => {
-//         setItems([]); // This function deletes all the items by setting the state to an empty array.
-//     };
-
-//     return (
-//         <div className="grocery-container">
-//             <h1>Grocery List</h1>
-//             <div className="input-section">
-//                 <input
-//                     type="text"
-//                     value={newItem}
-//                     onChange={(e) => setNewItem(e.target.value)}
-//                     placeholder="Enter new item"
-//                 />
-//                 <button onClick={addItem}>Add</button>
-//             </div>
-//             {items.length === 0 ? (
-//                 <p className="empty-list-message">There is nothing on your list!</p>
-//             ) : (
-//                 <ul className="grocery-list">
-//                     {items.map((item, index) => (
-//                         <li
-//                             key={index}
-//                             className={item.crossed ? 'crossed-out' : ''}
-//                             onClick={() => toggleItem(index)}
-//                         >
-//                             {item.name}
-//                         </li>
-//                     ))}
-//                 </ul>
-//             )}
-//             <DeleteBtn onDelete={deleteAllItems} />
-//         </div>
-//     );
-// };
-
-// export default GroceryList;
-
 import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './grocerylist.css';
@@ -86,6 +27,12 @@ const GroceryList = ({ items, setItems }) => {
         setItems(updatedItems);
     };
 
+    const removeItem = (index) => {
+        const updatedItems = [...items];
+        updatedItems.splice(index, 1);
+        setItems(updatedItems);
+    };
+
     const deleteAllItems = () => {
         setItems([]);
     };
@@ -111,15 +58,23 @@ const GroceryList = ({ items, setItems }) => {
                 <p className="empty-list-message">There is nothing on your list!</p>
             ) : (
                 <ul className="grocery-list">
-                    {items.map((item, index) => (
-                        <li
-                            key={index}
-                            className={item.crossed ? 'crossed-out' : ''}
-                            onClick={() => toggleItem(index)}
-                        >
-                            {item.name}
-                        </li>
-                    ))}
+                    {items.map((item, index) =>
+                        JSON.parse(item.name).map((ingredient, idx) => (
+                            <li key={index + idx} className={item.crossed ? 'crossed-out' : ''}>
+                                <label>
+                                    <input
+                                        type="checkbox"
+                                        checked={item.crossed}
+                                        onChange={() => toggleItem(index)}
+                                    />
+                                    {ingredient.trim()}
+                                </label>
+                                {item.crossed && (
+                                    <button onClick={() => removeItem(index)}>Remove</button>
+                                )}
+                            </li>
+                        ))
+                    )}
                 </ul>
             )}
             <DeleteBtn onDelete={deleteAllItems} />
--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -2,7 +2,6 @@ import "./list.css"
 import GroceryList from "../../components/grocerylist/grocerylist";
 
 const List = () => {
-    // Fetch grocery items from local storage and parse them
     const storedGroceryItems = localStorage.getItem('groceryItems');
     const groceryItems = storedGroceryItems ? JSON.parse(storedGroceryItems) : [];
 
